671a4bbdfb5986e9d09c004191ba2b202e1a6400
Henrique Rosan <henriquerosan@gmail.com>
2026-06-13 09:19:11 -0300

    Feat: Testing type cast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # INITIALIZING VARIABLES
-user_name = input("Write your name: ") # The user should write his/her name
-user_age = input("What's you age: ") # The user should write his/her age.
+user_name: str = input("Write your name: ") # The user should write his/her name
+user_age: int = int(input("What's you age: ")) # The user should write his/her age. The int function will do th type casting
+
+future_age: int = user_age + 5 # Giving the age five years in the future
 
 # SHOWING THE INFORMATIONS
 print(f"Hello my name is {user_name}, I have {user_age} yrs") # Showing the information to the user
+print(f"In five years I'll be {future_age} old")
