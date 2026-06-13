9adc5682c229f4f21022e313f44c470247db9374
Henrique Rosan <henriquerosan@gmail.com>
2026-06-13 09:06:27 -0300

    Feat: Initial variables script!

---
+++ main.py
@@ -0,0 +1,6 @@
+# INITIALIZING VARIABLES
+user_name = input("Write your name: ") # The user should write his/her name
+user_age = input("What's you age: ") # The user should write his/her age.
+
+# SHOWING THE INFORMATIONS
+print(f"Hello my name is {user_name}, I have {user_age} yrs") # Showing the information to the user
